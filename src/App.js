f9b84389f9897c56d8e4d52065a37ffa42362bb2
Nuschhz <nusch05@hotmail.com>
2023-01-04 21:05:45 -0300

    My first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Buttons } from './components/Buttons';
+import './App.css';
+
+function App() {
+  return (
+    <div className='Page'>
+        <Buttons />
+    </div>
+  );
+}
+
+export default App;
