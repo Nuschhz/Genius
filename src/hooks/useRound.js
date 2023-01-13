37aa8b74ad77352f731aabece902570b18aa0c78
Nuschhz <nusch05@hotmail.com>
2023-01-13 19:36:48 -0300

    changed the hover opacity again

--- src/hooks/useRound.js
+++ src/hooks/useRound.js
@@ -1,11 +1,7 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 export const useRound = () => {
   const [round, setRound] = useState(0);
 
-  const sumRound = useCallback(() => {
-    setRound(round + 1);
-  }, [round]);
-
-  return [round, setRound, sumRound];
+  return [round, setRound];
 };
